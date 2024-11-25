c8923c1e64b7e3ec2efcfce55e94a5db81d6b6b3
Manshi <kumarimanshi814@gmail.com>
2024-11-25 22:16:54 +0530

    polymorphism

--- src/Inheritence.java
+++ src/Inheritence.java
@@ -1,3 +1,14 @@
 public class Inheritence {
+public static void main(String args[]){
+    System.out.println("inheritence");
+    B objectofA = new B();
+    objectofA.property="abc";
+    System.out.println(objectofA.property);
+}
+}
+class A{
+    String property;
+}
+class B extends A{
 
 }
